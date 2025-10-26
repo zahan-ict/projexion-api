73a59d58bdaf596fbfdce0d28f6ecd03672af59a
Sarwar Zahan <sarwar.zahan@outlook.de>
2025-10-26 12:02:55 +0100

    Feat: Update dashboard preview policy

--- src/main/java/com/projexion/api/dashboard/DashboardService.java
+++ src/main/java/com/projexion/api/dashboard/DashboardService.java
@@ -2,6 +2,9 @@
  * Copyright (c) 2025 ProjeXion. All rights reserved.
  */
 package com.projexion.api.dashboard;
+import com.projexion.api.Project.ProjectEntity;
+import com.projexion.api.company.CompanyEntity;
+import com.projexion.api.contact.ContactEntity;
 import com.projexion.api.user.UserEntity;
 import io.quarkus.hibernate.reactive.panache.common.WithSession;
 import io.smallrye.mutiny.Uni;
@@ -16,10 +19,24 @@ public class DashboardService {
     @WithSession
     public Uni<Map<String, Long>> getTotals() {
         Map<String, Long> totals = new HashMap<>();
+
         return UserEntity.count()
-                .map(userCount -> {
+                .flatMap(userCount -> {
                     totals.put("total_users", userCount);
+                    return ContactEntity.count();
+                })
+                .flatMap(contactCount -> {
+                    totals.put("total_contacts", contactCount);
+                    return ProjectEntity.count();
+                })
+                .flatMap(projectCount -> {
+                    totals.put("total_projects", projectCount);
+                    return CompanyEntity.count();
+                })
+                .map(companyCount -> {
+                    totals.put("total_company", companyCount);
                     return totals;
                 });
     }
 }
+
